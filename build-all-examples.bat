ba50661682e6cd1e2bdf7396ec583abc46fef9c2
MrTurtle <mrturtle815@gmail.com>
2026-08-12 10:28:22 -0600

    Updated for new moveablePlayer2D.cpp

--- build-all-examples.bat
+++ build-all-examples.bat
@@ -4,10 +4,13 @@
 
 if not exist bin mkdir bin
 
-echo [1/2] compiling spinningShapes2D.cpp
+echo [1/3] compiling spinningShapes2D.cpp
 g++ examples\spinningShapes2D.cpp -o bin\spinningShapes2D.exe -lopengl32 -lgdi32 -luser32
 
-echo [2/2] compiling spinningShapes3D.cpp
+echo [2/3] compiling spinningShapes3D.cpp
 g++ examples\spinningShapes3D.cpp -o bin\spinningShapes3D.exe -lopengl32 -lgdi32 -luser32
+
+echo [3/3] compiling moveablePlayer2D.cpp
+g++ examples\moveablePlayer2D.cpp -o bin\moveablePlayer2D.exe -lopengl32 -lgdi32 -luser32
 
 pause
