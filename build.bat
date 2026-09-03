f97b44fbda87052408e7780918105dab2fd5b681
MrTurtle <mrturtle815@gmail.com>
2026-09-03 17:30:02 -0600

    Added libs for G++ bat script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: currently the only way to compile the project is to run this .bat file although i do plan to add cmake in the future!
 
 if not exist bin mkdir bin
-g++ src/main.cpp -o bin/Klomble
+g++ src/Application/main.cpp -o bin/Klomble -Ilibraries/include -Llibraries/lib -lraylib -lopengl32 -lgdi32 -lwinmm
 pause
