c938ba76a4ac3e1dd7f94f851919c99d5bd2833a
MrTurtle <mrturtle815@gmail.com>
2026-09-06 15:01:47 -0600

    Added rlImGui & ImGui to libraries

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 :: currently the only way to compile the project is to run this .bat file although i do plan to add cmake in the future!
 
 if not exist bin mkdir bin
-g++ src/Application/main.cpp -o bin/Klomble -Ilibraries/include -Llibraries/lib -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ src/Application/main.cpp src/Application/application.cpp src/Render/render.cpp ^
+imgui-files/rlImGui.cpp imgui-files/imgui_demo.cpp imgui-files/imgui_draw.cpp imgui-files/imgui_tables.cpp imgui-files/imgui_widgets.cpp imgui-files/imgui.cpp ^
+-o bin/Klomble -Ilibraries/include -Llibraries/lib ^
+-lraylib -lopengl32 -lgdi32 -lwinmm
 pause
